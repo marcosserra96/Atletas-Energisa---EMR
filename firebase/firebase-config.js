9e8669e40ccce55e45723a63bcfe46e66e6bee67
marcosserra96 <marcospauloserra@outlook.com.br>
2025-10-31 12:46:45 -0300

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,12 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC2l8LU3vYfQjTly8JSa658mfIlVk2Dw8E",
   authDomain: "inovacao-emr.firebaseapp.com",
   projectId: "inovacao-emr",
-  storageBucket: "inovacao-emr.appspot.com", // 🔹 corrigido aqui
+  storageBucket: "inovacao-emr.firebasestorage.app",
   messagingSenderId: "1075399271811",
   appId: "1:1075399271811:web:f532f25547125d6a8f42b6",
   measurementId: "G-8CTLMNCZJN"
@@ -14,5 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
 export { app };
