ae7fabedc1419192a524bc25f6f2908cebd2da52
marcosserra96 <marcospauloserra@outlook.com.br>
2025-10-31 08:14:23 -0300

    Create firebase-config.js

---
+++ firebase/firebase-config.js
@@ -0,0 +1,16 @@
+// 🔹 Arquivo: /firebase/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
