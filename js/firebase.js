972aae5e7ab0ea754df0646f66f96240df88888e
marcosserra96 <marcospauloserra@outlook.com.br>
2026-02-06 08:30:55 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { 
-  getFirestore, collection, getDocs, doc, getDoc, setDoc, updateDoc, addDoc, deleteDoc 
+  getFirestore, collection, getDocs, doc, getDoc, setDoc, updateDoc, addDoc, deleteDoc,
+  query, where, orderBy // <--- ADICIONADOS PARA FILTROS
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 import { 
   getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut 
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
-// --- SUA CONFIGURAÇÃO ---
 const firebaseConfig = {
   apiKey: "AIzaSyC2l8LU3vYfQjTly8JSa658mfIlVk2Dw8E",
   authDomain: "inovacao-emr.firebaseapp.com",
@@ -16,16 +16,13 @@ const firebaseConfig = {
   appId: "1:1075399271811:web:f532f1d6fa2b21c53c2ff3"
 };
 
-// 1. Inicia o App
 const app = initializeApp(firebaseConfig);
-
-// 2. Prepara os serviços
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// 3. Exporta TUDO (Incluindo deleteDoc que faltava)
 export { 
   app, db, auth, 
   collection, getDocs, doc, getDoc, setDoc, updateDoc, addDoc, deleteDoc,
+  query, where, orderBy,
   signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut 
 };
